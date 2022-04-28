de05e78615726e61df1f907d50a344d0084c0fcb
Brenon Paul <bp.brenon@gmail.com>
2022-04-28 11:56:17 -0300

    Ínicio do projeto de kanban em Vue JS

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script>
+import ListaTarefas from "./components/ListaTarefas.vue";
+export default {
+  components: { ListaTarefas },
+  data() {
+    return {
+      titulo: "Kanban",
+      tarefas: [
+        {
+          descricao: "treinar",
+        },
+        {
+          descricao: "estudar",
+        },
+        {
+          descricao: "jogar",
+        },
+      ],
+    };
+  } 
+};
+</script>
+<template>
+    <h1>{{ titulo }}</h1> 
+    <main>
+      <ListaTarefas title="To Do" :tarefas="tarefas"/>
+      <ListaTarefas title="Done" :tarefas="tarefas"/>
+    </main>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+body {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  background-color: powderblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+main {
+  width: 60vw;
+  background-color: azure;
+  height: 70vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 10px;
+  padding: 1%;
+}
+
+h1 {
+  text-align: center;
+}
+
+</style>
---
+++ src/components/ListaTarefas.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+        },
+        tarefas: {
+            type: Object,
+            default: [],
+        }
+    }
+};
+</script>
+<template>
+    <article>
+        <h2>{{ title }}</h2>
+        <section>
+            <div v-for="(tarefa, index) in tarefas" :key="index">{{tarefa.descricao}}
+            <button>ok</button>
+            </div>
+        </section>
+    </article>
+</template>
+<style>
+article {
+    width: 45%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+section {
+    background-color: rgb(127, 207, 218);
+    width: 100%;
+    height: 50vh;
+    border-radius: 5px;
+}
+</style>
